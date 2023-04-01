0b3ef986e9ece33250613f798c184e81e7267319
Dipadana Putu <dipadanadev@gmail.com>
2023-04-01 08:14:15 +0800

    Update basic crud logic

--- README.md
+++ README.md
@@ -2,20 +2,32 @@
 
 ### Step-step
 
-1. Make sure you have `node` and `npm` installed in your computer.
+1. Make new folder.
+
+```sh
+mkdir 01-express-101
+```
+
+2. Open the folder.
+
+```sh
+cd 01-express-101
+```
+
+3. Make sure you have `node` and `npm` installed in your computer.
 
 ```sh
 node -v
 npm -v
 ```
 
-2. Init `npm` for start the project.
+4. Init `npm` for start the project.
 
 ```sh
 npm init
 ```
 
-3. For now, just press `enter` if you asked to fill something. If done, file `package.json` will appeared in your directory.
+5. For now, just press `enter` if you asked to fill something. If done, file `package.json` will appeared in your directory.
 
 ```json
 {
@@ -31,17 +43,27 @@ npm init
 }
 ```
 
-4. Install the express
+6. Install the express
 
 ```sh
 npm install express
 ```
 
-5. Create new file `index.js` and copy this script.
+7. Create new file `index.js`
+
+```sh
+touch index.js
+```
+
+8. Copy this script.
 
 ```js
 const express = require("express");
-let app = express();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
@@ -52,10 +74,10 @@ app.listen(3000, () => {
 });
 ```
 
-6. Try to run the script, and open `localhost:3000`.
+9. Try to run the script, and open `localhost:3000`.
 
 ```sh
 node index.js
 ```
 
-7. Improve your script!
+10. Improve your script!
--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 const express = require("express");
-let app = express();
 
-let persons = ["Budi", "Dipadana", "Anto", "Dode", "Theo"]
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.json({ message: "Hello express" });
+});
+
+let persons = [
+  { id: 1, name: "Budi" },
+  { id: 2, name: "Dipadana" },
+  { id: 3, name: "Anto" },
+  { id: 4, name: "Theo" }
+]
+
+app.get("/persons", (req, res) => {
   res.json({ persons: persons });
 });
+
+app.get("/person/:id", (req, res) => {
+  const findPerson = persons.find((data) => {
+    return data.id === Number(req.params.id)
+  })
+
+  res.json({ person: findPerson });
+});
+
+app.post("/person", (req, res) => {
+  const newPerson = {
+    id: persons[persons.length-1].id + 1,
+    name: req.body.name
+  }
+
+  persons.push(newPerson)
+
+  res.json({ person: newPerson });
+})
+
+app.put("/person/:id", (req, res) => {
+  const personIndex = persons.findIndex((data) => {
+    return data.id === Number(req.params.id)
+  })
+
+  persons[personIndex].name = req.body.name
+
+  res.json({ person: persons[personIndex] });
+})
+
+app.delete("/person/:id", (req, res) => {
+  const filteredPerson = persons.filter((data) => {
+    return data.id !== Number(req.params.id)
+  })
+
+  persons = filteredPerson
+
+  res.json({ message: `Data with id ${req.params.id} has been deleted` });
+})
 
 app.listen(3000, () => {
   console.log("Running on localhost:3000");
